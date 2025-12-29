92ad48ef16172f09defd6579ad836d85e2c5c43d
Dogo6647 <diegobaezgg@gmail.com>
2025-12-29 14:14:47 -0700

    feat: change feedback url to SE! discord

--- src/lib/brand.js
+++ src/lib/brand.js
@@ -2,6 +2,6 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
     APP_NAME: 'ScratchBox Editor',
-    FEEDBACK_URL: 'https://github.com/ScratchBox-SE',
+    FEEDBACK_URL: 'https://discord.gg/Y2gf5vZHpJ',
     GITHUB_URL: 'https://github.com/ScratchBox-SE'
 };
