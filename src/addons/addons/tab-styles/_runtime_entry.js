17443f05b05c60ee73fb39e814505435647c6824
Grady Link <github@grady.link>
2026-02-11 16:15:33 -0800

    feat: add scratchbox compat to tab styles addon

--- src/addons/addons/tab-styles/_runtime_entry.js
+++ src/addons/addons/tab-styles/_runtime_entry.js
@@ -1,7 +1,7 @@
 import _turbowarpCSS from "!css-loader!./turbowarp.css";
-import _scratchboxCSS from "!css-loader!./scratchbox.css";
+import _mistwarpCSS from "!css-loader!./mistwarp.css";
 
 export const resources = {
     "turbowarp.css": _turbowarpCSS,
-    "scratchbox.css": _scratchboxCSS,
+    "mistwarp.css": _mistwarpCSS,
 };
